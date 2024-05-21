aff6fcb3a02dc377a8e846a2e2cdfa47cd10bab9
Maaz Nadeem <maaznadeem007@outlook.com>
2024-05-22 02:12:55 +0500

    header and navbar

--- home.js
+++ home.js
@@ -1,6 +1,22 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
+document.getElementById('burger').addEventListener('click', function() {
+  document.getElementById('mobileMenu').classList.toggle('active');
+});
 
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
+document.getElementById('closeMenu').addEventListener('click', function() {
+  document.getElementById('mobileMenu').classList.remove('active');
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
+  
+  window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY;
+      const viewportHeight = window.innerHeight;
+      
+      // When the page is scrolled down by at least half of the viewport height
+      if (scrollY > viewportHeight / 2) {
+          header.classList.add('slide-up');
+      } else {
+          header.classList.remove('slide-up');
+      }
+  });
 });
